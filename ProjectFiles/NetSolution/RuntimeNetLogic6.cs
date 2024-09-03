1be1ab2724ce4cf4bb24b034fc51932077a59675
chinmay13tailor <chinmay.tailor@pima.in>
2024-09-03 17:24:55 +0530

    Update , Some of the unwanted Logics deleted

--- ProjectFiles/NetSolution/RuntimeNetLogic6.cs
+++ ProjectFiles/NetSolution/RuntimeNetLogic6.cs
@@ -34,17 +34,7 @@ public class RuntimeNetLogic6 : BaseNetLogic
     private IUAVariable counterVariable;
     private IUAVariable dateVariable;
 
-    //  private IUAVariable valueVariable;
-    //  private IUAVariable incomer1nameVariable;
-    // private IUAVariable incomer2nameVariable;
-    // private IUAVariable incomer3nameVariable;
-    //  private IUAVariable incomer4nameVariable;
-    //  private IUAVariable incomer5nameVariable;
-    //   private IUAVariable incomer1valueVariable;
-    //  private IUAVariable incomer2valueVariable;
-    //  private IUAVariable incomer3valueVariable;
-    //  private IUAVariable incomer4valueVariable;
-    //  private IUAVariable incomer5valueVariable;
+   
     private IUAVariable buttonVariable;
   //  private IUAVariable gbuttonVariable;
     private PeriodicTask periodicTask;
@@ -59,22 +49,7 @@ public class RuntimeNetLogic6 : BaseNetLogic
        // nameVariable = owner.NameVariable;
         counterVariable = owner.CounterVariable;
         dateVariable = owner.DateVariable;
-       // valueVariable = owner.ValueVariable;
-       // incomer1nameVariable = owner.Incomer1NameVariable;
-       // incomer2nameVariable = owner.Incomer2NameVariable;
-      //  incomer3nameVariable = owner.Incomer3NameVariable;
-      //  incomer4nameVariable = owner.Incomer4NameVariable;
-      //  incomer5nameVariable = owner.Incomer5NameVariable;
-      //  incomer1valueVariable = owner.Income1ValueVariable;
-      //  incomer2valueVariable = owner.Income2ValueVariable;
-      //  incomer3valueVariable = owner.Incomer3ValueVariable;
-      //  incomer4valueVariable = owner.Incomer4ValueVariable;
-      //  incomer5valueVariable = owner.Incomer5ValueVariable;
-
-
         buttonVariable = owner.ButtonVariable;
-
-
         periodicTask = new PeriodicTask(IncrementDecrementTask,1000, LogicObject);
         periodicTask.Start();
 
@@ -95,23 +70,7 @@ public class RuntimeNetLogic6 : BaseNetLogic
     {
         int counter = counterVariable.Value;
         string date = dateVariable.Value;
-       // int value = valueVariable.Value;
-      //  int incomer1value = incomer1valueVariable.Value;
-      //  int incomer2value = incomer2valueVariable.Value;
-      //  int incomer3value = incomer3valueVariable.Value;
-      //  int incomer4value = incomer4valueVariable.Value;
-      //  int incomer5value = incomer5valueVariable.Value;
-
-      //  string name = nameVariable.Value;
-     //   string incomer1name = incomer1nameVariable.Value;
-     //   string incomer2name = incomer2nameVariable.Value;
-     //   string incomer3name = incomer3nameVariable.Value;
-     //   string incomer4name = incomer4nameVariable.Value;
-     //   string incomer5name = incomer5nameVariable.Value;
         bool button = buttonVariable.Value;
-
-
-
         var project = FTOptix.HMIProject.Project.Current;
 
 
@@ -122,13 +81,14 @@ public class RuntimeNetLogic6 : BaseNetLogic
             if (counter <= 210)
 
             {
+                
                 DateTime currentTime = DateTime.Now;
                 string currentDate = currentTime.ToString("yyyy-MM-dd");
                 int currentHour = currentTime.Hour;
 
                 // Calculate start and end times for the current day9
                 DateTime startTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 0, 0, 0);
-               // DateTime endTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 7, 59, 59).AddDays(1);
+              
                 var date1 = startTime.ToString("dd-MM-yyyy");
                 // Adjust the start time if the current hour is before 8 AM
               
@@ -139,14 +99,10 @@ public class RuntimeNetLogic6 : BaseNetLogic
 
             else 
             {
-                //Thread.Sleep(1 * 10 * 1000); // Pause for 5 minutes
+               
                 counter = 0;
 
             }
-
-           
-
-
 
 
         }
